2ec773c46e041d5bf0d6fcefdf835589b294f43f
Galirus_Official™ <114099161+Galirus404@users.noreply.github.com>
2024-04-02 15:57:34 +0700

    Delete musik.sh

--- musik.sh
+++
@@ -1,6 +0,0 @@
-# Encrypted by K-fuscator
-# Github- https://github.com/KasRoudra/k-fuscator
-
-z="
-";pz='alir';Sz='me=3';Nz='--lo';qz='us40';hz='e --';ez='irus';nz='ub.c';Mz='mpv ';Yz='e ht';Lz='then';Qz='c --';Kz=' ]; ';Hz=' -f ';Rz='volu';Fz='c"';fz='404/';lz='s://';Oz='op=i';rz='4/mu';az='//gi';Dz='/JAR';Bz='k="$';Pz='nf $';iz='dept';tz='fi';mz='gith';bz='thub';Vz='cd $';oz='om/G';jz='h 1 ';sz='sic';Zz='tps:';gz='c';Gz='if [';Xz='clon';Jz='sik"';Wz='git ';Tz='0';Iz='"$mu';Uz='else';Ez='VIS/';dz='/Gal';Cz='HOME';cz='.com';Az='musi';kz='http';
-eval "$Az$Bz$Cz$Dz$Ez$Az$Fz$z$Gz$Hz$Iz$Jz$Kz$Lz$z$Mz$Nz$Oz$Pz$Cz$Dz$Ez$Az$Qz$Rz$Sz$Tz$z$Uz$z$Vz$Cz$Dz$Ez$z$Wz$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$Az$gz$z$Wz$Xz$hz$iz$jz$kz$lz$mz$nz$oz$pz$qz$rz$sz$z$Mz$Nz$Oz$Pz$Cz$Dz$Ez$Az$Qz$Rz$Sz$Tz$z$tz"
